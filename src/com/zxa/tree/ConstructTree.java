c0e5ab0168da243ec2b4686fa9528ebdbb644046
shixi006 <xin_anzhang@126.com>
2017-08-04 16:25:11 +0800

    Initial commit

---
+++ src/com/zxa/tree/ConstructTree.java
@@ -0,0 +1,25 @@
+package com.zxa.tree;
+
+/**
+ * Created by shixi006 on 2017/8/4.
+ */
+public class ConstructTree {
+    
+    class Node<T>{
+        
+        public T root;
+        Node left;
+        Node right;
+        
+        public Node(T data){
+            this.root = data;
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+}
